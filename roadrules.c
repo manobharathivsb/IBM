e14c345f1b45cc1b0a38c3a1faa0b9bdc728f8a1
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-03-07 15:01:24 +0000

    Update roadrules.c

--- roadrules.c
+++ roadrules.c
@@ -7,44 +7,29 @@ int main(void)
 	scanf("%d\t%d\t%d\t%d\n",&licence,&insurance,&rcbook,&helmet);
 	scanf("%d\t%d",&highspeed,&morethan2persons);
 	{
-	if(n==1)
-	{if(licence==0)
-	{printf("paid no fine\n");
-		}
-		else
-		{	printf("paid rupees %d\n",a+1000);
+	{
+	if(licence==0)
+	{
+		a=a+1000;
 		}
 		if(insurance==0)
-		{printf("paid no fine\n");
-		}
-		else
-		{printf("paid rupees %d\n",a+500);
+		{a=a+500;
 		}
 		if(rcbook==0)
-		{printf("paid no fine\n");
-		}
-		else
-		{printf("paid rupees %d\n",a+700);
+		{a=a+700;
 		}
 		if(helmet==0)
-		{printf("paid no fine\n");
-		}
-		else
-		{printf("paid rs %d\n",a+400);
+		{a=a+400;
 		}
 		if(highspeed==0)
-	{printf("paid no fine\n");
-	}
-	else
-	{printf("paid Rs %d\n",a+350);
+	{a=a+350;
 	}
 	if(morethan2persons==0)
-	{printf("paid no fine\n");
-	}
-	else
-	{printf("paid rs %d\n",a+300);
+	{a=a+300;
 	}}
-	else
+	if((licence==1||insurance==1||rcbook==1||helmet==1||highspeed==1||morethan2person==1)
+         {
 	printf("congrates he/she follow all the rules");
-return 0;
+	 }
+	return 0;
 }}
