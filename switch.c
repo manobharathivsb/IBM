c8fed920248646f6ab273a06b036a32f6275dac7
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-03-10 11:27:30 +0000

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(void) 
+{int a,b;
+char c;
+printf("enter the number");
+scanf("%d",&a);
+switch(a)
+{
+	case '1':
+	printf("bill details");
+	break;
+	case '2':
+	printf("talk plan");
+	break;
+	case '3':
+	printf("deals and discount");
+	break;
+	case '4':
+	printf("internet plan details");
+	break:
+	case '5':
+	printf("voice sms & roaming packs");
+	break;
+	case '6':
+	printf("bill pay recharge");
+	break;
+	case '7':
+	printf("best offer");
+	break;
+	case '8'
+	printf("what's new");
+	break;
+	default:
+	printf("no choice")
+	return 0;
+}
+	
