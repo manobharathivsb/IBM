7057b9389a8a90b6e51640a831c358a219166b97
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-03-24 18:04:44 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+	int a,i,j,c,d,diff=0,sum=0;
+    char b[50],e[50];
+    scanf("%s",b);
+    scanf("%s",e);
+    printf("%d",a);  // Reading input from STDIN
+   for(i=0;i<=10;i++)
+   {
+       printf("%d",b);
+       c=a-b;
+   }
+   sum=sum+c;
+   printf("%d",sum);
+    for(j=0;j<=10;j++)
+    {
+        printf("%d",e);
+        d=a-e;
+    }
+    diff=diff+d;
+    printf("%d",diff);
+    for(i=0;i<=10;i++)
+    {
+        if(c==d)
+{
+    count++;
+}
+printf("%d",count);
+    }
+ return 0;   
+}
